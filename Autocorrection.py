f9d1956e3a4794883451e4bea0776ecd665eb8e8
rawan68 <rawanmohy68@gmail.com>
2021-11-08 20:12:47 +0200

    stemming update

--- Autocorrection.py
+++ Autocorrection.py
@@ -2,12 +2,14 @@ import nltk
 
 #nltk.download()
 import pandas as pd
+from nltk.stem import PorterStemmer
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 data = pd.read_csv('news_quiz_qa-dev.csv')
 questions = data['question']
 answers = data['answer']
+ps = PorterStemmer()
 #answers_arr = answers.to_numpy()
 #ex_sentence = answers[0]
 stop_words = set(stopwords.words("English"))
@@ -18,4 +20,8 @@ for i in range(len(answers)):
     for word in sentence:
         if word not in stop_words:
             filtered_sentence.append(word)
+    
+    for word in sentence:
+        print(word, " : ", ps.stem(word))
+       
     print(filtered_sentence)
